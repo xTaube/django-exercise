2fdadedcd4597d4e7594ce8d1d2a1e97e934b443
Michal <micmey9@gmail.com>
2021-06-30 23:36:30 +0200

    add authentication

--- pokemon/api/views.py
+++ pokemon/api/views.py
@@ -2,6 +2,8 @@ from rest_framework import generics
 from pokemon.models import Pokemon
 from .serializers import PokemonSerializer
 from rest_framework import filters
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework import permissions
 
 class PokemonAPIView(generics.ListCreateAPIView):
     serializer_class = PokemonSerializer
@@ -10,6 +12,8 @@ class PokemonAPIView(generics.ListCreateAPIView):
     queryset = Pokemon.objects.all()
 
 class PokemonDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
     serializer_class = PokemonSerializer
     queryset = Pokemon.objects.all()
     lookup_field = 'id'
